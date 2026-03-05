ac43e123effcfa2dbe82b5db935d9a9bd553aa5d
Paul McCarthy <pauldmccarthy@gmail.com>
2026-03-05 15:30:00 +0000

    Add version check into test scripts

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,2 +1,5 @@
+# check that package version is correct
+# (true evalutes to 1, so we use !=)
+python -c "import sys; import os; import indexed_gzip as igz; sys.exit(igz.__version__ != os.environ['PKG_VERSION'])"
 python -m indexed_gzip.tests -c pyproject.toml -m "not slow_test"          --pyargs indexed_gzip
 python -m indexed_gzip.tests -c pyproject.toml -m "not slow_test" --concat --pyargs indexed_gzip
